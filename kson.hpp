b69eb1ab73570ba4517031dd9da8350a56c3e13d
Kevin Lu <69993704+kevlu8@users.noreply.github.com>
2023-09-15 11:55:59 -0700

    Create kson.hpp

---
+++ kson.hpp
@@ -0,0 +1,11 @@
+#include <vector>
+#include <string>
+
+class json {
+private:
+	// internal structure here
+public:
+	void load_string(std::string jstring) {
+		// { "hi": "0", "die": 1, }
+	}
+};
